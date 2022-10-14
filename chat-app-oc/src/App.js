9fe0c44b1b69b424552c4f7f13346b0a9a902855
[razamuzammil426@gmail.com] <razamuzammil426@gmail.com>
2022-10-14 15:40:12 +0500

    feat react-router functionality

--- chat-app-oc/src/App.js
+++ chat-app-oc/src/App.js
@@ -2,12 +2,23 @@ import './style.scss'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Home from './pages/Home'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Register />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Home />} />
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
